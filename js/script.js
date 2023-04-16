e0277216e2f25b4ab3ede73cfd816ee3b280deea
Aryan <aryanrajput039@gmail.com>
2023-04-16 09:10:34 +0530

    Did a lot of changes

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Initialize Swiper 
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    centeredSlides: true,
+    spaceBetween: 30,
+    grabCursor: true,
+    autoplay: {
+        delay: 1000,
+        pauseOnMouseEnter: true,
+    }
+});
